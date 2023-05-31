135dea80e7d70aa64b5cb74a36bb432c7fdb4f86
ottojoash <louisjoshbricks@gmail.com>
2023-05-31 12:47:03 +0300

    navbar

--- react-social/src/home/Home.js
+++ react-social/src/home/Home.js
@@ -1,30 +1,29 @@
 import React, { Component } from 'react';
 import './Home.css';
+import { iconName } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes, faQrcode, faLink, faStream, faCalendarWeek, faQuestionCircle, faSlidersH, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 class Home extends Component {
     render() {
         return (
-            <div className="home-container">
-                <div className="container">
-                    <div className="graf-bg-container">
-                        <div className="graf-layout">
-                            <div className="graf-circle"></div>
-                            <div className="graf-circle"></div>
-                            <div className="graf-circle"></div>
-                            <div className="graf-circle"></div>
-                            <div className="graf-circle"></div>
-                            <div className="graf-circle"></div>
-                            <div className="graf-circle"></div>
-                            <div className="graf-circle"></div>
-                            <div className="graf-circle"></div>
-                            <div className="graf-circle"></div>
-                            <div className="graf-circle"></div>
-                        </div>
-                    </div>
-                    <h1 className="home-title">Spring Boot React OAuth2 Social Login Demo</h1>
-                </div>
+            <div>
+              
+              <div className="sidebar">
+                <header>BI-FAM</header>
+                <ul>
+                  <li><a href="#"><FontAwesomeIcon icon={faQrcode} />Dashboard</a></li>
+                  <li><a href="#"><FontAwesomeIcon icon={faLink} />Shortcuts</a></li>
+                  <li><a href="#"><FontAwesomeIcon icon={faStream} />Overview</a></li>
+                  <li><a href="#"><FontAwesomeIcon icon={faCalendarWeek} />Events</a></li>
+                  <li><a href="#"><FontAwesomeIcon icon={faQuestionCircle} />About</a></li>
+                  <li><a href="#"><FontAwesomeIcon icon={faSlidersH} />Services</a></li>
+                  <li><a href="#"><FontAwesomeIcon icon={faEnvelope} />Contact</a></li>
+                </ul>
+              </div>
+              <section></section>
             </div>
-        )
+          );
     }
 }
 
